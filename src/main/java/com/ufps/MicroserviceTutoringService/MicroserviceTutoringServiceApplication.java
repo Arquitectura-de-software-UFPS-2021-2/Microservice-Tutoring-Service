afef4deb2f7b407ed0371999e1f739fb1d78d73d
bsvega <bsvega@bancolombia.com.co>
2021-11-10 19:19:23 -0500

    initial commit

---
+++ src/main/java/com/ufps/MicroserviceTutoringService/MicroserviceTutoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ufps.MicroserviceTutoringService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTutoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTutoringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufps/MicroserviceTutoringService/MicroserviceTutoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufps.MicroserviceTutoringService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTutoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
