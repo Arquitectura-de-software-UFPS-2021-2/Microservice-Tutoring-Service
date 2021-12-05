61d1d0fe884ed32cb8267560808c276ccd304d86
danielpinzon <danielfernandopp@ufps.edu.co>
2021-12-05 10:52:43 -0500

    arreglo de endpoints de categoria

--- src/main/java/com/ufps/microservice/tutoring/tutoring/aplicacion/manejador/categoria/ManejadorGuardarCategorias.java
+++ src/main/java/com/ufps/microservice/tutoring/tutoring/aplicacion/manejador/categoria/ManejadorGuardarCategorias.java
@@ -4,8 +4,6 @@ import com.ufps.microservice.tutoring.tutoring.dominio.modelo.Categoria;
 import com.ufps.microservice.tutoring.tutoring.dominio.repositorio.CategoriaRepositorioInterface;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
 
@@ -14,14 +12,14 @@ public class ManejadorGuardarCategorias {
 
     private final CategoriaRepositorioInterface categoriaRepositorioInterface;
 
-    public ResponseEntity<Categoria> guardar(Categoria categoria) throws NotFoundException {
-        Optional<Categoria> categoriaBusqueda = categoriaRepositorioInterface.findId(categoria.getId());
+    public Categoria guardar(Categoria categoria) throws NotFoundException {
+        Categoria categoriaGuardar = categoria;
+        categoriaRepositorioInterface.save(categoriaGuardar);
+        Optional<Categoria> categoriaBusqueda = categoriaRepositorioInterface.findId(categoriaGuardar.getId());
         if (categoriaBusqueda.isEmpty()){
             throw new NotFoundException("No se encontro la categoria");
         }
-        categoriaBusqueda.get().actualizarCategoria(categoria.getId(), categoria.getName());
-        categoriaRepositorioInterface.save(categoriaBusqueda.get());
-        return new ResponseEntity<>(HttpStatus.OK);
+        return categoriaBusqueda.get();
     }
 
 
