5a9505a18c2606ad99f2c44cf62cd8e2ade6aaa9
bsvega <bsvega@bancolombia.com.co>
2021-12-06 22:35:28 -0500

    enable feign client

--- src/main/java/com/ufps/microservice/tutoring/MicroserviceTutoringServiceApplication.java
+++ src/main/java/com/ufps/microservice/tutoring/MicroserviceTutoringServiceApplication.java
@@ -2,8 +2,10 @@ package com.ufps.microservice.tutoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MicroserviceTutoringServiceApplication {
 
     public static void main(String[] args) {
